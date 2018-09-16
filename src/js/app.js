d9e7cddf8e8cfc4e6f2de9f47004ba8833a1a5b1
Ruslana Kholod <RuslanaKholod@gmail.com>
2018-09-16 22:02:49 +0300

    add footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,19 @@
 $( document ).ready(function() {
-  $('.slides').slick({
-   slidesToShow: 1,
-   swipeToSlide: true,
-   arrows: false,
-   dots: true
- });
+
+  if ($('.slides').length > 0) {
+    $('.slides').slick({
+     slidesToShow: 1,
+     swipeToSlide: true,
+     arrows: false,
+     dots: true
+   });
+  }
+  if ($('.team-slides').length > 0) {
+    $('.team-slides').slick({
+     slidesToShow: 1,
+     swipeToSlide: true,
+     arrows: false,
+     dots: true
+     });
+  }
 });
--- src/js/app2.js
+++ src/js/app2.js
@@ -1,1 +1,88 @@
-console.log("app 2")
+customPlaceholderInit();
+
+function customPlaceholderInit() {
+  var els = $('.custom-placeholder-wrap');
+  els.each(function () {
+      $(this).on('click', clickHandler);
+      $(this).find('input, textarea').on('focus', focusHandler);
+      $(this).find('a[data-event]').on('click', clickControl);
+  });
+
+  textareaDetect();
+
+  function textareaDetect() {
+      els.each(function () {
+          var textarea = $(this).find('textarea');
+          if (textarea.length) {
+              $(this).find('.custom-placeholder').addClass('textarea-custom-placeholder');
+          }
+      });
+  }
+
+  function clickControl(e) {
+      var el = findParent($(e.target), 'custom-placeholder-wrap');
+      el.addClass('custom-placeholder-active');
+  }
+
+  function clickHandler(e) {
+      var el = findParent($(e.target), 'custom-placeholder-wrap'),
+          input = el.find('input, textarea');
+      el.addClass('custom-placeholder-active');
+      input
+          .focus()
+          .focusout(function () {
+              var self = $(this),
+                  val = self.val().trim();
+
+              if (!val && !$(e.target).hasClass('datepicker-form')) {
+
+                  el.removeClass('custom-placeholder-active');
+
+              } else if (!val && $(e.target).hasClass('datepicker-form')) {
+
+                  var that = $(this)
+                  $(document).on('click', function () {
+
+                      setTimeout(function () {
+                          if (!that.val().trim() && !$('.datepicker-container').is(':visible')) {
+                              el.removeClass('custom-placeholder-active');
+                          }
+                      }, 200);
+                  })
+              }
+          });
+  }
+
+  function focusHandler(e) {
+      var el = findParent($(e.target), 'custom-placeholder-wrap');
+      el.addClass('custom-placeholder-active');
+
+      $(e.target).focusout(function () {
+          var val = $(this).val().trim();
+          if (!val && !$(e.target).hasClass('datepicker-form')) {
+              el.removeClass('custom-placeholder-active');
+          } else if (!val && $(e.target).hasClass('datepicker-form')) {
+
+              var that = $(this);
+              $(document).on('click', function () {
+                  setTimeout(function () {
+                      if (!that.val().trim() && !$('.datepicker-container').is(':visible')) {
+                          el.removeClass('custom-placeholder-active');
+                      }
+                  }, 200);
+              })
+          }
+      });
+  }
+}
+
+function findParent(el, class_) {
+  var parent = el.parent();
+  // debugger;
+  if (parent.hasClass(class_)) {
+      return parent;
+  }
+  else {
+      return findParent(parent, class_);
+  }
+}
